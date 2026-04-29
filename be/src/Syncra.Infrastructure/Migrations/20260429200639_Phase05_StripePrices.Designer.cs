e0f254cec80adbb090021e9f9d5ae72c7fde407a
Syncra Dev <dev@syncra.local>
2026-04-30 03:09:05 +0700

    feat(05-01): add and apply EF Core migration for Stripe prices

--- be/src/Syncra.Infrastructure/Migrations/20260429200639_Phase05_StripePrices.Designer.cs
+++ be/src/Syncra.Infrastructure/Migrations/20260429200639_Phase05_StripePrices.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Syncra.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Syncra.Infrastructure.Persistence;
 namespace Syncra.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260429200639_Phase05_StripePrices")]
+    partial class Phase05_StripePrices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ be/src/Syncra.Infrastructure/Migrations/20260429200639_Phase05_StripePrices.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Syncra.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Phase05_StripePrices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_plans_stripe_price_id",
+                table: "plans");
+
+            migrationBuilder.RenameColumn(
+                name: "stripe_price_id",
+                table: "plans",
+                newName: "stripe_yearly_price_id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "stripe_monthly_price_id",
+                table: "plans",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "plans",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                columns: new[] { "stripe_monthly_price_id", "stripe_yearly_price_id" },
+                values: new object[] { "price_placeholder_free", null });
+
+            migrationBuilder.UpdateData(
+                table: "plans",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                columns: new[] { "stripe_monthly_price_id", "stripe_yearly_price_id" },
+                values: new object[] { "price_placeholder_pro", null });
+
+            migrationBuilder.UpdateData(
+                table: "plans",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"),
+                columns: new[] { "stripe_monthly_price_id", "stripe_yearly_price_id" },
+                values: new object[] { "price_placeholder_team", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_plans_stripe_monthly_price_id",
+                table: "plans",
+                column: "stripe_monthly_price_id",
+                unique: true,
+                filter: "stripe_monthly_price_id IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_plans_stripe_yearly_price_id",
+                table: "plans",
+                column: "stripe_yearly_price_id",
+                unique: true,
+                filter: "stripe_yearly_price_id IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_plans_stripe_monthly_price_id",
+                table: "plans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_plans_stripe_yearly_price_id",
+                table: "plans");
+
+            migrationBuilder.DropColumn(
+                name: "stripe_monthly_price_id",
+                table: "plans");
+
+            migrationBuilder.RenameColumn(
+                name: "stripe_yearly_price_id",
+                table: "plans",
+                newName: "stripe_price_id");
+
+            migrationBuilder.UpdateData(
+                table: "plans",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "stripe_price_id",
+                value: "price_placeholder_free");
+
+            migrationBuilder.UpdateData(
+                table: "plans",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "stripe_price_id",
+                value: "price_placeholder_pro");
+
+            migrationBuilder.UpdateData(
+                table: "plans",
+                keyColumn: "id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"),
+                column: "stripe_price_id",
+                value: "price_placeholder_team");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_plans_stripe_price_id",
+                table: "plans",
+                column: "stripe_price_id",
+                unique: true,
+                filter: "stripe_price_id IS NOT NULL");
+        }
+    }
+}
